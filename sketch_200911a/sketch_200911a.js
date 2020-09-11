01b4686b7e69e46d668844da281bc6ffcb7f3dc9
Jund Jae Hwan <woghks7413@naver.com>
2020-09-11 14:45:50 +0900

    Create sketch_200911a.js

---
+++ sketch_200911a/sketch_200911a.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(500,500)
+  
+  fill(250,0,0);
+  ellipse(100,100,100,100);
+  fill(255,255,0);
+  ellipse(200,100,100,100);
+  fill(0,255,0);                                        
+  ellipse(100,200,100,100);
+  fill(0,0,255);
+  ellipse(200,200,100,100);
+  fill(255,0,255);
+  ellipse(300,300,100,100);
+}
+
+
+function draw() {
+
+}
