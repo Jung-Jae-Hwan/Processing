5ad93a06be5c953f25767ce6da14ec04fffc39d1
Jund Jae Hwan <woghks7413@naver.com>
2020-09-18 23:05:36 +0900

    Jung-Jae-Hwan

---
+++ sketch_200918a/sketch_200918a.js
@@ -0,0 +1,14 @@
+  function setup() {
+  createCanvas(500, 500);
+  for(let i = 0; i<=500; i += 10){
+    line(i, 0, i, 500);
+  for(let j = 0; j<=500; j += 10){
+    line(0, j, 500, j);
+    
+}
+  }
+  }
+ function draw() {
+ 
+
+ }
