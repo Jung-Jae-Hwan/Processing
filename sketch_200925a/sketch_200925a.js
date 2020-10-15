729f1a30f4d15c70e127e60af11b67f71596a39e
Jund Jae Hwan <woghks7413@naver.com>
2020-10-15 23:04:55 +0900

    Jung-Jae-Hwan

--- sketch_200925a/sketch_200925a.js
+++ sketch_200925a/sketch_200925a.js
@@ -1,9 +1,16 @@
 function setup() {
- //I am 11125 jungjaehwan
+  createCanvas(500, 500);
+  for(let i=0; i<500; i+=size){
+    for(let j=0; j<500; j+=size){
+      let size = int(random(0, 100));
+      
 
+      
+      
+    }           
+  }
 }
 
 
-function draw() {
 
-}
+  
