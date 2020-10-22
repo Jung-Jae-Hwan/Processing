f4a55d569f71415f84665dc13d1d464336902c78
Jund Jae Hwan <woghks7413@naver.com>
2020-10-22 23:34:29 +0900

    Jung-Jae-Hwan

---
+++ sketch_200925a/sketch_201016a/sketch_201016a.js
@@ -0,0 +1,29 @@
+let state = 0;
+
+function setup(){
+  createCanvas(500, 500);
+}
+
+function draw(){
+  if(state == 0){
+  background(255);
+  noStroke();
+  fill(0);
+  ellipse(mouseX, mouseY, 50, 50);
+}
+  else{
+    background(0);
+    noStroke();
+    fill(255);
+    ellipse(mouseX, mouseY, 50, 50);
+  }
+}
+
+function mousePressed(){
+   if( state == 0){
+      state = 1;
+   }
+   else{
+      state = 0;
+   }
+}
